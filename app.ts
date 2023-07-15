f10cb4e7ced24d443c7cf9f4a6981aab3f923f87
hareeshsprasad <hareeshsprasad@gmail.com>
2023-07-15 19:24:50 +0530

    creating the super admin on server start completed

--- app.ts
+++ app.ts
@@ -1,30 +1,56 @@
-import express from 'express';
-var bodyParser = require('body-parser')
-const fileUpload = require("express-fileupload")
-const path = require('path')
-const util = require('util')
-var cors = require('cors')
+import express from "express";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
+import * as bcrypt from "bcrypt";
+import cors from "cors";
+import { Sequelize, DataTypes } from "sequelize";
+import apiRouter from "./src/router";
+import { indexFunction } from "./src/modules/index";
+import { User } from "./src/modules/user/user.model";
 const app = express();
-const Config = require('config');
-const port = Config.get('port');
-import apiRouter from './src/router';
-import {indexFunction} from './src/modules/index'
+const Config = require("config");
+const port = Config.get("port");
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(cors());
-app.use('/', apiRouter);
-app.use('/',indexFunction);
+app.use("/", apiRouter);
+app.use("/", indexFunction);
 
 /* for log api request start */
-import morganBody from 'morgan-body';
+import morganBody from "morgan-body";
 morganBody(app);
 /* for log api request end */
-// app.use(express.static('public')); 
-// app.use('/uploads', express.static('uploads/productImage'));
 
-
-app.listen(port, ()=>{
-    console.log(`Connected successfully on port ${port}`)
+const sequelize = new Sequelize({
+  dialect: "mysql",
+  host: Config.get("database.mysql.host"),
+  username: Config.get("database.mysql.username"),
+  password: Config.get("database.mysql.password"),
+  database: Config.get("database.mysql.dbName"),
+});
+let Password = "superadmin@123";
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync(Password, salt);
+Password = hash;
+User.findOrCreate({
+    where: { Email: 'SuperAdmin@gmail.com' },
+    defaults: {
+      Name: 'SuperAdmin',
+      Password: Password,
+      Role: 'SuperAdmin',
+    },
+  }).then(([user, created]) => {
+  if (created) {
+    console.log('User created:', user.get());
+  } else {
+    console.log('User already exists:', user.get());
+  }
+  app.listen(port, () => {
+    console.log(`Connected successfully on port ${port}`);
+  });
 })
+.catch((error) => {
+  console.error('Unable to start the server:', error);
+});
